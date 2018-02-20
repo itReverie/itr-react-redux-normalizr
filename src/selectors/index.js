3396914c63d00d885bcde8ec069bd011e8be1124
itReverie <brenda@itreverie.com>
2018-02-19 19:36:07 -0500

    Adding the correct immutable query on the selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,40 +3,16 @@ import Immutable from 'immutable';
 import { Map } from 'immutable';
 
 const getQuestions = state => {
-  console.log('SELECTOR: ',state);
-  state.getIn(['entities', 'questions']);}
+                              let theTempState=Immutable.fromJS(state);//TOASK: Is it ok to use FromJs?
+                              //console.log('SELECTOR fromJS:',theTempState);
+                              let pregGet=theTempState.get('questions')
+                              console.log('SELECTOR GET:',pregGet);
+                              //let pregGetIn=theTempState.getIn(['entities', 'questions'])
+                              //console.log('SELECTOR GETIN:',pregGetIn);
+                              return pregGet;
+                              };
 
 export const getQuestionsResult = createSelector(
   [ getQuestions ],
   (questions) => {  return questions }
 );
-// const getItem = (state, id) => state.byId[id];
-// const getQuestions = (state, questionId) => state.parts.get('byQuestionId');
-// const getItemsIds = state => state.ids;
-// const getQuestionsIds = (state) => {
-//   state.questions.get('byQuestionId').get('')};
-// // const getItemsById = state => state.byId;
-// const getQuestionsById = (state) => state.get('byId');
-//
-// //export
-//  const getQuestionsSelector = createSelector(
-//   [getQuestionsIds, getQuestionsById],
-//   // (itemsIds, itemsById) => itemsIds.map(id => itemsById[id]),
-//   (questionsIds, questionsById) =>
-//   questionsIds.map(id => questionsById.get(id)),
-// );
-
-
-// export default function getQuestions(state)
-// {
-  // let state = Immutable.fromJS(state);
-  // //console.log('SELECTOR: state ',state);
-  // let taskSelector = state => state.get('questions');
-  // //console.log('SELECTOR: questions ',taskSelector);
-  // export const allQuestionsSelector = createSelector(
-  //     [ taskSelector],
-  //     questions => {  return questions.map();}
-  // );
-  //console.log('SELECTOR: new questions ',allQuestionsSelector);
-  //return allQuestionsSelector;
-//}
