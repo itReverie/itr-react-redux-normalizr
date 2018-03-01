ed91c7a715021c465f43284bcc0b4e0fa218683e
itreverie <brendadhk@gmail.com>
2018-03-01 17:58:27 -0500

    Adding a solution by adding onSelectResetsInput={false}

--- src/components/Questions/Selector.js
+++ src/components/Questions/Selector.js
@@ -69,7 +69,7 @@ onInputChange (newTextValue)
         value={this.props.part.toJS().text}
         onChange={this.onChange}
         onInputChange={this.onInputChange}
-                         onSelectResetsInput={false}
+        onSelectResetsInput={false}
       />
     )
   }
